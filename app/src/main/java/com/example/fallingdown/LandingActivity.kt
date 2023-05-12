1080f80307fa312d366f252bc5fe68456ff0ec77
zoozxc456 <zoozxc456@gmail.com>
2023-05-12 22:43:51 +0800

    add login layout slide up&dwon animation and add login&sign up buttons in the layout

--- app/src/main/java/com/example/fallingdown/LandingActivity.kt
+++ app/src/main/java/com/example/fallingdown/LandingActivity.kt
@@ -1,11 +1,29 @@
 package com.example.fallingdown
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ActionMenuView.LayoutParams
+import androidx.appcompat.app.AppCompatActivity
 
 class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var loginView = findViewById<View>(R.id.login_bottom_sheet)
+        updateLoginViewHeight(loginView)
+    }
+
+    private fun updateLoginViewHeight(loginView: View) {
+        loginView.post {
+            val height = loginView.height
+            val params = loginView.layoutParams
+            val offsetOfParent = 50
+
+            params.height = (height - offsetOfParent)
+            params.width = LayoutParams.MATCH_PARENT
+            loginView.layoutParams = params
+        }
     }
 }
