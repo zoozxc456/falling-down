745a6c080e6c387948713a930ce5444c01022826
MingYu <zoozxc456@gmail.com>
2023-05-13 23:03:15 +0800

    add login layout exit button and listen bottom sheet behavior state

--- app/src/main/java/com/example/fallingdown/LandingActivity.kt
+++ app/src/main/java/com/example/fallingdown/LandingActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ActionMenuView.LayoutParams
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,25 @@ class LandingActivity : AppCompatActivity() {
 
         var loginView = findViewById<View>(R.id.login_bottom_sheet)
         updateLoginViewHeight(loginView)
+
+        var closeLoginViewBtn = findViewById<Button>(R.id.btn_close_loginView)
+        var bottomBehavior = BottomSheetBehavior.from(loginView)
+
+        bottomBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED){
+                    closeLoginViewBtn.visibility = View.INVISIBLE
+                }else if (newState == BottomSheetBehavior.STATE_EXPANDED){
+                    closeLoginViewBtn.visibility = View.VISIBLE
+                }
+            }
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+        })
+
+        closeLoginViewBtn.setOnClickListener {
+            bottomBehavior.isHideable=true
+            bottomBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        }
     }
 
     private fun updateLoginViewHeight(loginView: View) {
