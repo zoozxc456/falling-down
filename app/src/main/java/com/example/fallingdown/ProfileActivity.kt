00f2e7f4ece6f0dd0bf37d1aa3f862981c9efedc
zoozxc456 <zoozxc456@gmail.com>
2023-05-22 18:36:40 +0800

    add profile back to user application layout action

--- app/src/main/java/com/example/fallingdown/ProfileActivity.kt
+++ app/src/main/java/com/example/fallingdown/ProfileActivity.kt
@@ -2,10 +2,16 @@ package com.example.fallingdown
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val backToPreviousActivityButton = findViewById<ImageView>(R.id.btn_profile_back_page)
+        backToPreviousActivityButton.setOnClickListener {
+            finish()
+        }
     }
 }
